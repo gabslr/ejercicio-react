b5b2a36ce5d4d0005738eff6299386fc96d67809
gabslr <gaby.lopez.rojas@gmail.com>
2024-07-09 14:55:19 -0600

    modificación Testimonio js y css

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "../hojas-de-estilo/Testimonio.css"
 
-function Testimonio() {
+function Testimonio(props) {
   return(
     <div className="contenedor-testimonio">
       <img 
       className="imagen-testimonio"
-      src={require('../imagenes/witkin.png')}
+      src={require(`../imagenes/${props.imagen}.png`)}
       alt="Foto de Witkin"
       />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Peter Witkin en Estados Unidos</p>
-        <p className="cargo-testimonio">Fotógrafo artístico</p>
-        <p className="texto-testimonio">La historia del arte ha sido trazada siempre por creadores originales, por verdaderos autores capaces de crear piezas que trascienden su propio tiempo, son artistas que no les importa tratar de complacer a nadie, comprenden que ese no es el propósito de su creación.</p>
+        <p className="nombre-testimonio"><strong>{props.nombre}</strong> {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo}</p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
   );
@@ -19,3 +20,7 @@ function Testimonio() {
 
 
 export default Testimonio
+
+
+
+
